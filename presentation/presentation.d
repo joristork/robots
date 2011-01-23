f59a1f3a63cb91392ece27879ba8c6a2923e705b
joristork <joris@wintermute.eu>
2011-01-23 22:52:44 +0100

    Almost there......

---
+++ presentation/presentation.d
@@ -0,0 +1,150 @@
+# vim: ft=make
+.PHONY: presentation._graphics
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/ifxetex/ifxetex.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/atbegshi.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/bigintcalc.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/bitset.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/etexcmds.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/gettitlestring.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/ifluatex.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/ifpdf.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/ifvtex.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/infwarerr.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/intcalc.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/kvsetkeys.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/ltxcmds.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/pdfescape.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/pdftexcmds.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/generic/oberdiek/uniquecounter.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/amscls/amsthm.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/amsfonts/amsfonts.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/amsfonts/amssymb.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/amsmath/amsbsy.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/amsmath/amsgen.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/amsmath/amsmath.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/amsmath/amsopn.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/amsmath/amstext.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamer.cls
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaseauxtemplates.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaseboxes.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasecolor.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasecompatibility.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasedecode.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasefont.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaseframe.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaseframecomponents.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaseframesize.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaselocalstructure.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasemisc.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasemodes.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasenavigation.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasenotes.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaseoptions.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaseoverlay.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasercs.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaserequires.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasesection.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasetemplates.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasethemes.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasetheorems.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasetitle.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasetoc.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasetranslator.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbasetwoscreens.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/beamerbaseverbatim.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/themes/color/beamercolorthemedefault.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/themes/font/beamerfontthemedefault.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/themes/inner/beamerinnerthemedefault.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/themes/outer/beamerouterthemedefault.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/themes/outer/beamerouterthemesidebar.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/themes/theme/beamerthemeGoettingen.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/themes/theme/beamerthemedefault.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/translator/translator-language-mappings.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/beamer/translator/translator.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/geometry/geometry.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/graphics/graphics.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/graphics/graphicx.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/graphics/keyval.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/graphics/trig.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/hyperref/hyperref.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/hyperref/nameref.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/ms/everyshi.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/atveryend.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/epstopdf-base.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/grfext.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/hycolor.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/kvoptions.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/letltxmacro.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/refcount.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/rerunfilecheck.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/oberdiek/xcolor-patch.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/tools/enumerate.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf-texlive-dist/tex/latex/xcolor/xcolor.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcore.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorearrows.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcoreexternal.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcoregraphicstate.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcoreimage.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorelayers.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcoreobjects.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorepathconstruct.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorepathprocessing.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorepathusage.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorepatterns.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorepoints.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorequick.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcorescopes.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcoreshade.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcoretransformations.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/basiclayer/pgfcoretransparency.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmath.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathcalc.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfloat.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfunctions.base.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfunctions.basic.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfunctions.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfunctions.comparison.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfunctions.misc.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfunctions.random.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfunctions.round.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathfunctions.trigonometric.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathparser.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/math/pgfmathutil.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/systemlayer/pgfsys.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/systemlayer/pgfsysprotocol.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/systemlayer/pgfsyssoftpath.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/utilities/pgfkeys.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/utilities/pgfkeysfiltered.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/utilities/pgfrcs.code.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/generic/pgf/utilities/pgfutil-common.tex
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/latex/html/url.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/latex/pgf/basiclayer/pgfcore.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/latex/pgf/systemlayer/pgfsys.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/latex/pgf/utilities/pgfrcs.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: /opt/local/share/texmf/tex/latex/pgf/utilities/xxcolor.sty
+presentation.aux presentation.aux.make presentation.d presentation.pdf: presentation.tex
+.SECONDEXPANSION:
+-include blueprint.png.gpi.d
+presentation.d: $$(call graphics-source,blueprint.png)
+presentation.pdf presentation._graphics: $$(call graphics-target,blueprint.png)
+-include google_pizzas.png.gpi.d
+presentation.d: $$(call graphics-source,google_pizzas.png)
+presentation.pdf presentation._graphics: $$(call graphics-target,google_pizzas.png)
+-include metric.png.gpi.d
+presentation.d: $$(call graphics-source,metric.png)
+presentation.pdf presentation._graphics: $$(call graphics-target,metric.png)
+-include topological.png.gpi.d
+presentation.d: $$(call graphics-source,topological.png)
+presentation.pdf presentation._graphics: $$(call graphics-target,topological.png)
+-include voxels.png.gpi.d
+presentation.d: $$(call graphics-source,voxels.png)
+presentation.pdf presentation._graphics: $$(call graphics-target,voxels.png)
+-include streetpolygon.png.gpi.d
+presentation.d: $$(call graphics-source,streetpolygon.png)
+presentation.pdf presentation._graphics: $$(call graphics-target,streetpolygon.png)
+-include edgeordering.png.gpi.d
+presentation.d: $$(call graphics-source,edgeordering.png)
+presentation.pdf presentation._graphics: $$(call graphics-target,edgeordering.png)
+-include voorbeeld.png.gpi.d
+presentation.d: $$(call graphics-source,voorbeeld.png)
+presentation.pdf presentation._graphics: $$(call graphics-target,voorbeeld.png)
---
+++ presentation/presentation.toc
@@ -0,0 +1,8 @@
+\beamer@endinputifotherversion {3.10pt}
+\beamer@sectionintoc {1}{Robots and maps}{3}{0}{1}
+\beamer@sectionintoc {2}{Sensorial maps}{10}{0}{2}
+\beamer@subsectionintoc {2}{1}{Image based mapping}{11}{0}{2}
+\beamer@subsectionintoc {2}{2}{Spacial occupancy representations}{14}{0}{2}
+\beamer@subsectionintoc {2}{3}{Geometric maps}{22}{0}{2}
+\beamer@sectionintoc {3}{Topological Maps}{28}{0}{3}
+\beamer@sectionintoc {4}{Multiple Robots}{34}{0}{4}
