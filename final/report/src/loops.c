a57ba2e314edae495e635b776721c460423a3f6c
Sander van Veen <sandervv@gmail.com>
2011-01-30 21:16:36 +0100

    Improved 'Cobot's data receiver'.

---
+++ final/report/src/loops.c
@@ -0,0 +1,13 @@
+void main() {
+    int i;
+
+    // Normal loop
+    for(i = 0; i < 3; i++) {
+        printf("i = %d\n", i);
+    }
+
+    // Reverse loop
+    for(i = 2; i >= 0; i--) {
+        printf("i = %d\n", i);
+    }
+}
